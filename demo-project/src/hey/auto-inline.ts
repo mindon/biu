2c3a20652a695d73621b665b7fe1708d88aeb46c
mindon <mindon@gmail.com>
2026-05-14 10:50:13 +0800

    fix

--- demo-project/src/hey/auto-inline.ts
+++ demo-project/src/hey/auto-inline.ts
@@ -2,3 +2,7 @@ export const hello = `biu html v1778640066`;
 
 import { TIMESTAMP2 } from "./world2.ts";
 console.log(TIMESTAMP2);
+
+// import { str } from "@lit/localize";
+// console.log(str);
+// need to run biu second time
